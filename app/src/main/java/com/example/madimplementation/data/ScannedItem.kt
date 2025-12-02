4bcfa88ed6d52c11c296788b8f822a41ed681075
Sean Byrne O Riain <S00254331@atu.ie>
2025-12-02 19:55:21 +0000

    Partial Room Implementation

---
+++ app/src/main/java/com/example/madimplementation/data/ScannedItem.kt
@@ -0,0 +1,13 @@
+package com.example.madimplementation.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ScannedItem(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val brand: String?,
+    val model: String?,
+    val imagePath: String?,
+    val timestamp: Long = System.currentTimeMillis()
+)
